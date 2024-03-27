a8187c609bd7fda4ae1d44becd29a9360559a5b7
muqimbayev <muqimbayev@gmail.com>
2024-03-27 12:51:57 +0500

    Swagger yaratildi

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant_app',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg',
+    'rest_framework_simplejwt',
 
 ]
 
